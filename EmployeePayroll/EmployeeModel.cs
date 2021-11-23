7bbec5f12d69b2bade3429ca15ebef4a87e3a489
swapnil-bagul <bagulswapnilp@gmail.com>
2021-11-23 05:43:16 +0530

    [Swapnil] Added UC1 Create Database & connect with program

---
+++ EmployeePayroll/EmployeeModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeePayroll
+{
+    class EmployeeModel
+    {
+        public int EmplyeeID { get; set; }
+        public string  EmplyeeName { get; set; }
+        public double PhoneNumber { get; set; }
+        public double  Salary { get; set; }
+        public string  Address { get; set; }
+        public string  Department { get; set; }
+        public string  Gender { get; set; }
+        public double  BasicPay { get; set; }
+        public double Deduction { get; set; }
+        public double TaxablePay { get; set; }
+        public double IncomeTax { get; set; }
+        public double NetPay { get; set; }
+        public DateTime  Startdate { get; set; }
+
+    }
+}
---
+++ EmployeePayroll/EmployeeRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using System.Data.SqlClient;
+namespace EmployeePayroll
+{
+    class EmployeeRepository
+    {
+        public static string connectionString = @"Data Source = (localdb)\ProjectsV13;Initial Catalog = Payroll_Services_Database;";
+        SqlConnection connection = new SqlConnection(connectionString);
+    }
+}
